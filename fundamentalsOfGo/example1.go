cfd2f24b3bcdd6ae49c4ff5c438b5cdebb350714
arai <Ankur.Rai@viasat.com>
2024-12-28 12:05:49 +0000

    New: Added all folders

---
+++ fundamentalsOfGo/example1.go
@@ -0,0 +1,10 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(`printing some raw  string literals which is an 
+	concatination of severals characters `)
+}
